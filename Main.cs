c2925a1d3f8833b2b844cd6484c17480db264315
bigsk8man <51745804+bigsk8man@users.noreply.github.com>
2022-10-05 16:05:42 +0200

    mets le message

--- Main.cs
+++ Main.cs
@@ -1,11 +1,14 @@
 ﻿Pizza margarita = new Pizza("Margarita", 10, 10, "Tomato sauce, Mozzarella");
-
 Drink coca = new Drink("Coca-cola", 25, 3.99);
-
-Client jimmy = new Client("Jimmmy", "Potard", 015, 02012001)
+Client jimmy = new Client("Jimmmy", "Potard", 015, "4 mars 2022");
 
 coca.PrintDrink();
 
 coca.Price = 2.55;
 
 coca.PrintDrink();
+
+
+jimmy.PrintClient();
+
+Console.WriteLine(coca.Price);
