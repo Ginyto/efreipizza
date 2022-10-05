01c1001529f06fa1b5f18f98a0195f9a080b22ec
Ginyto <agianlucca@gmail.com>
2022-10-05 15:28:18 +0200

    Tout fonctionne

--- Main.cs
+++ Main.cs
@@ -1,5 +1,11 @@
-﻿Pizza margherita = new Pizza("Margherita", 7.99, 32, "Tomato sauce, mozzarella, basil");
+﻿Console.WriteLine("Hello there!");
 
+Pizza margarita = new Pizza("Margarita", 10, 10, "Tomato sauce, Mozzarella");
 
+Pizza pepperoni = new Pizza("Pepperoni", 10, 10, "Tomato sauce, Mozzarella, Pepperoni");
 
-margherita.PrintPizza();
+Pizza hawaiian = new Pizza("Hawaiian", 10, 10, "Tomato sauce, Mozzarella, Ham, Pineapple");
+
+margarita.PrintPizza();
+pepperoni.PrintPizza();
+hawaiian.PrintPizza();
