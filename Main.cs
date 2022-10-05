75a7953abada9f222b69fcc3a73b993e677bb0f2
Ginyto <agianlucca@gmail.com>
2022-10-05 15:21:08 +0200

    test ignore

--- Main.cs
+++ Main.cs
@@ -1,5 +1,5 @@
 ﻿Pizza margherita = new Pizza("Margherita", 7.99, 32, "Tomato sauce, mozzarella, basil");
 
-Drink coca = new Drink("Coca-cola,", 50, 3.50);
 
-coca.PrintDrink();
+
+margherita.PrintPizza();
