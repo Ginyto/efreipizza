522564b963fe634ee6326ee4ee0808f3e0a049e0
bigsk8man <51745804+bigsk8man@users.noreply.github.com>
2022-10-09 17:31:53 +0200

    Merge branch 'entity' of https://github.com/Ginyto/efreipizza into entity

--- Command.cs
+++ Command.cs
@@ -3,31 +3,88 @@ namespace Projet
 
     public class Command
     {
+        public int OrderNumber { get; set; }
+        public string Time { get; set; }
+        public string Date { get; set; }
+        public string CommisName { get; set; }
         public string ClientName { get; set; }
         public string ClientFirstName { get; set; }
-        public string TypeOfCommand { get; set; }
+        public List<string> Pizzas { get; set; }
+        public List<string> Drinks { get; set; }
         public double PriceOrder { get; set; }
+        public int State { get; set; }  //state 0 = prise en charge commande , 1 = préparation commande , 2 = en livraison , 3 = livrée
 
-        public Command(string ClientName, string ClientFirstName, string TypeOfCommand,double PriceOrder)
+
+
+        public Command(int ordernumber, string time, string date, string commisname,string clientname, string clientfirstname, List<string> pizzas, List<string> drinks, double priceorder, int state)
         {
-            this.ClientName=ClientName;
-            this.ClientFirstName=ClientFirstName;
-            this.TypeOfCommand=TypeOfCommand;
-            this.PriceOrder=PriceOrder;
+            this.OrderNumber=ordernumber;
+            this.Time=time;
+            this.Date=date;
+            this.CommisName=commisname;
+            this.ClientName=clientname;
+            this.ClientFirstName=clientfirstname;
+            this.Pizzas=pizzas;
+            this.Drinks=drinks;
+            this.PriceOrder=priceorder;
+            this.State=state;          
             
         }
 
         public void PrintCommand()
-    {
-        Console.WriteLine($"ClientName: {ClientName}");
-        Console.WriteLine($"ClientFirstName: {ClientFirstName}");
-        Console.WriteLine($"TypeOfCommand: {TypeOfCommand}");   
-        Console.WriteLine($"PriceOrder: {PriceOrder}");   
+    {    
+        Console.WriteLine($"\nCommand number : {OrderNumber}");
+
+        switch(State) {
+            case 0:
+                Console.WriteLine($"State : In preparation");
+                break;
+            case 1:
+                Console.WriteLine($"State : In delivery");
+                break;
+            case 2:
+                Console.WriteLine($"State : Delivered");
+                break;
+
+            default : 
+                Console.WriteLine($"Lost command");
+                break;           
+        }
+
+        Console.WriteLine($"------------------------------------------");
+        Console.WriteLine($"Time : {Time}");
+        Console.WriteLine($"Date : {Date}");
+        Console.WriteLine($"ClerckName : {CommisName}");
+        Console.WriteLine($"ClientName : {ClientName}");
+        Console.WriteLine($"ClientFirstName : {ClientFirstName}");
+
+        Console.WriteLine($"------------------------------------------");
+
+        Console.WriteLine($"Pizzas :");
+        foreach(var piz in Pizzas)
+            Console.WriteLine($"- {piz}"); 
+        Console.WriteLine($"------------------------------------------");
+        Console.WriteLine($"Drinks :");
+        foreach(var dri in Drinks)
+            Console.WriteLine($"- {dri}"); 
+        Console.WriteLine($"------------------------------------------");
+        Console.WriteLine($"PriceOrder : {PriceOrder} $ \n");   
     }
+
+
+
+
      public double MakeCommand(){
         return PriceOrder;
 
      }
     }
+
+
+    public class ListOfCommand
+    {
+        public List<Command> CommandList { get; set; }
+    }
     
 }
+
--- Pizza.cs
+++ Pizza.cs
@@ -23,5 +23,3 @@ class Pizza
     }
 
 }
-
-
