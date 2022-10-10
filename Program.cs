34c9d00bd452875d789783132685b76d6b416a79
Ginyto <agianlucca@gmail.com>
2022-10-10 22:18:22 +0200

    trying a new namespace

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using efreipizza;
-
+using Projet;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
